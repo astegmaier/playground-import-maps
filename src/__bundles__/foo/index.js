7c3451b9b0b6de296848f0fb08a3f52bd7765fbd
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-07-21 13:13:26 -0400

    Show output in the browser

--- src/__bundles__/foo/index.js
+++ src/__bundles__/foo/index.js
@@ -1,2 +1,3 @@
 import { Version } from 'bar';
-console.log(Version); // This should log 'CORRECT - Version 2' from the bar module
+const outputElement = document.getElementById('output');
+outputElement.textContent = Version; // This should be 'CORRECT - Version 2'
