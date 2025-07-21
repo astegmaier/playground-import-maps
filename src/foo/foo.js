4076cecb910bfbb08cca7dcb88e3a705a022641d
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-07-21 13:01:31 -0400

    Cleanup

--- src/foo/foo.js
+++
@@ -1,5 +0,0 @@
-import { bar } from 'bar';
-
-export const foo = 'this came from foo.js, but it was imported with the bare specifier "foo"';
-
-export const barFromFoo = `foo imported bar and go this: ${bar}`;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { foo, barFromFoo } from 'foo';
-import { bar } from 'bar';
-console.log('foo:', foo);
-console.log('barFromFoo:', barFromFoo);
-console.log('bar:', bar);
